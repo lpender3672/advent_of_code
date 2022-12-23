76a98e1ec6beeff432a0eae61397d0347e9f7b49
lpender3672 <louispender3672@gmail.com>
2022-12-23 15:34:47 +0000

    day 9 took too long

---
+++ 2022/9.1.py
@@ -0,0 +1,48 @@
+
+moves = []
+with open('2022/9.txt', 'r') as f:
+    for l in f:
+        moves.append(l.strip().split(' '))
+
+for m in moves:
+    m[1] = int(m[1])
+
+# start
+head = (0, 0)
+tail = (0, 0)
+visited = [tail]
+
+def get_direction(d):
+    if d == 'U':
+        return [0, 1]
+    elif d == 'D':
+        return [0, -1]
+    elif d == 'R':
+        return [1, 0]
+    elif d == 'L':
+        return [-1, 0]
+
+def sign(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
+head = (0, 0)
+tail = (0, 0)
+visited = set()
+visited.add(tail)
+
+for direction, steps in moves:
+    for _ in range(int(steps)):
+        d = get_direction(direction)
+        head = (head[0] + d[0], head[1] + d[1])
+        dx, dy = head[0] - tail[0], head[1] - tail[1]
+        if abs(dx) > 1 or abs(dy) > 1:
+            tail = (tail[0] + sign(dx), tail[1] + sign(dy))
+            visited.add(tail)
+
+
+print(len(visited))
